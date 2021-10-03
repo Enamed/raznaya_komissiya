1e4e25c97c8b90f120d137bca2fbed6cd665952c
Enamed <85908108+Enamed@users.noreply.github.com>
2021-10-03 13:51:12 +0300

    update homework

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,36 +1,30 @@
 fun main() {
-
     println("Для перевода денег, введите используемую банковскую карту ")
     println("1.VK Pay\n2.Mastercard/Maestro\n3.Visa/Мир")
-    val card = readLine()
-    println("Введите адресата(пишем что хотим)")
-    val adresat = readLine()
+    //val card = readLine()
+    val card = readLine()?.toDoubleOrNull() ?: 1
     println("Введите сумму предыдущих переводов за этот месяц")
-    val lastTrasaction = readLine()
+    val lastTrasaction = readLine()?.toDoubleOrNull() ?: 0
     println("Введите сумму перевода")
     val summa = readLine()
 
     val result = card?.toInt()?.let {
         lastTrasaction?.toDouble()
-            ?.let { it1 -> summa?.toDouble()?.let { it2 -> komission(it, adresat = String(), it1, it2) } }
+            ?.let { it1 -> summa?.toDouble()?.let { it2 -> komission(it, it1, it2) } }
     }
 }
 
-fun komission(card: Int, adresat: String, lastTrns: Double, summa: Double) {
-    //procent VISA
-    val procentsumm = (summa * 0.75) / 100
-    val minprocent = procentsumm + summa
-    val visaTransfer = if (procentsumm < 35.0) summa + 35.0 else minprocent
-    //procent MASTERCARD
-    val procentMastercard = ((summa * 0.6) / 100) + 20 + summa
-    val mastercardTransfer = if (summa > lastTrns) procentMastercard else summa
+fun komission(card: Int, lastTransaction: Double, summa: Double) {
 
     when (card) {
-        1 -> println("Сумма к переводу через VK Pay для $adresat составляет - $summa")
-        2 -> println("Сумма к переводу через Mastercard/Maestro для $adresat составляет - $mastercardTransfer")
-        3 -> println("Сумма к переводу через Visa/Мир для $adresat составляет - $visaTransfer")
+        1 -> println("Сумма к переводу через VK Pay составляет - $summa ")
+        2 -> println("Сумма к переводу через Mastercard/Maestro составляет - "+
+                if (summa > 75.000) ((summa * 0.6) / 100) + 20 + summa else summa)
+        3 -> println("Сумма к переводу через Visa/Мир составляет - " +
+                if ((summa * 0.75) / 100 < 35.0) summa + 35.0 else ((summa * 0.75) / 100) + summa)
     }
 }
+
 
 
 
